8ac3c608f3650942bf0d6f3e14a14b487e47121c
Mehul <vasavamehulkumar711@gmail.com>
2025-11-16 00:32:11 +0530

    fix(test): Update unit test mock to match JwtPayload interface

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -2,5 +2,4 @@ export * from './auth/jwt-auth.guard';
 export * from './auth/roles.decorator';
 export * from './auth/roles.guard';
 export * from './auth/jwt-payload.interface';
-// Add this line to export the decorator
 export * from './decorators/user.decorator';
